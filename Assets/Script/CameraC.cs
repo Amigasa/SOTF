efb6ab34944a1ff49866adf56c8ad5cb301d3120
Amigasa <kolya.s06@mail.ru>
2025-08-31 23:41:33 +0400

    Настроил границы камеры

---
+++ Assets/Script/CameraC.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraC : MonoBehaviour
+{
+    public Transform target;
+
+    public float minX, maxX;
+    public float minY, maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, minX, maxX), Mathf.Clamp(target.position.y, minY, maxY), transform.position.z);
+    }
+}
